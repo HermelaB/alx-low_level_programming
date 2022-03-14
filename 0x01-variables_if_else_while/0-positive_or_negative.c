f0efd8c5861d335316e91aa4843ae9a1b976462a
HermelaB <hermelabrhanu@gmail.com>
2022-03-14 08:35:31 -0700

    Task 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdliib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* Main-print wheather number
+*
+* Description: print wheather
+* Return:0
+*/
+int main(void)
+{
+
+	int n;
+	srand(time(0));
+	n=rand()-RAND_MAX / 2;
+
+	if (n>0)
+		printf("%i is positive\n",n);
+	else if (n==0)
+		printf("%i is zero\n",n);
+	else
+		printf(%i is negative\n", n);
+	return(0);
+}
