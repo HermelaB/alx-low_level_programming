135f853bfca92213e390140597426e47424056ad
HermelaB <hermelabrhanu@gmail.com>
2022-03-14 03:19:37 -0700

    my first task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+* Main print screen using print f
+*
+*
+* Return 0
+*
+*/
+int main(void)
+{
+	Printf("with proper grammar, but the outcome is a piecce of art,\n");
+	return(0);
+}
