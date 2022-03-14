4767c0dcad96831c5f1750a31f2def78142976d1
HermelaB <hermelabrhanu@gmail.com>
2022-03-14 08:54:44 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,6 @@
 #include <stdliib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
 * Main-print wheather number
 *
@@ -10,16 +9,16 @@
 */
 int main(void)
 {
-
 	int n;
-	srand(time(0));
-	n=rand()-RAND_MAX / 2;
 
-	if (n>0)
-		printf("%i is positive\n",n);
-	else if (n==0)
-		printf("%i is zero\n",n);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%i is positive\n", n);
+	else if (n == 0)
+		printf("%i is zero\n", n);
 	else
-		printf(%i is negative\n", n);
+		printf("%i is negative\n", n);
 	return(0);
 }
