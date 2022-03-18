cd8ad77b1c7f56d69529f214a33bd0139923d02a
HermelaB <hermelabrhanu@gmail.com>
2022-03-18 11:09:50 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+/**
+ * _isdigit - determin if digit
+ *
+ * @c: character to determin
+ *
+ * Return: gives boolean
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
