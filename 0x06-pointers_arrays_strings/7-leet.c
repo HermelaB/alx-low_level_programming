fa96e667f2b10455edc1818020c45ae75a4aa572
HermelaB <hermelabrhanu@gmail.com>
2022-03-24 10:38:01 -0700

    Task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,25 +9,17 @@
 */
 char *leet(char *s)
 {
-	int i;
+	int i, j;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; *(s + i); i++)
 	{
-	if (s[i] == 'a' || s[i] == 'A')
-		s[i] = '4';
-	else if (s[i] == 'e' || s[i] == 'E')
-		s[i] = '3';
-	else if (s[i] == 'o' || s[i] == 'O')
-		s[i] = '0';
-	else if (s[i] == 't' || s[i] == 'T')
-		s[i] = '7';
-	else if (s[i] == 'l' || s[i] == 'L')
-		s[i] = '1';
-	else
-		continue;
-
+		for (j = o; j <= 9; j++)
+		{
+			if (a[j] == *(s + i))
+				*(s + i) = b[j];
+		}
 	}
-
-
 	return (s);
 }
