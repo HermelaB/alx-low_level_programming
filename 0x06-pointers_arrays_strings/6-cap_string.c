d0030eb40a779d15ed792d51eeddb39a40b2e6aa
HermelaB <hermelabrhanu@gmail.com>
2022-03-24 11:00:41 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,33 +1,32 @@
 #include "main.h"
-/**
- * cap_string - capitalize after separator words
- * @s: character to capitalize after separator
- * Return: return value of upper
- */
 
+/**
+ * cap_string - capitalizes all words in a string
+ * @s: string to capitalize
+ *
+ * Return: address of s
+ */
 char *cap_string(char *s)
 {
-	int i, x;
-	int cap = 32;
-	int separators[] = {',', ';', '.', '?', '"', '(', ')', '{', '}', ' ', '\n', '\t'};
+	int i = 0, j;
+	char a[] = " \t\n,;.!?\"(){}";
 
-	for (i = 0; n[i] != '\0'; i++)
+	while (*(s + i))
 	{
-		if (n[i] >= 'a' && n[i] <= 'z')
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
 		{
-			n[i] = n[i] - cap;
-		}
-		
-		cap = 0;
-
-		for (x = 0; x <= 12; x++)
-		{
-			if (n[i] == separatprs[x])
+			if (i == 0)
+				*(s + i) -= 'a' - 'A';
+			else
 			{
-				x = 12;
-				cap = 32;
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(s + i - 1))
+						*(s + i) -= 'a' - 'A';
+				}
 			}
 		}
+		i++;
 	}
-	return (n);
+	return (s);
 }
