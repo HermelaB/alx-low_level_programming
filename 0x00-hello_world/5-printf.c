4d7427aa973b764416e46b417b2ca4ce1f0c0567
HermelaB <hermelabrhanu@gmail.com>
2022-03-25 11:50:11 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
-* Main function print f
-*
-*
-* Return 0
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
