1861328ee9f172797aa46ac8ab1e32d273e6235a
HermelaB <hermelabrhanu@gmail.com>
2022-03-25 11:43:58 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
+
 /**
-*main - print string from function
-*
-*Description: using main function 
-*Rreturn: 0
-*/
+ * main - print string from function
+ *
+ * Description: using main print string function
+ * Rreturn: Always 0 (success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
-return (0);
+	
+	return (0);
 }
