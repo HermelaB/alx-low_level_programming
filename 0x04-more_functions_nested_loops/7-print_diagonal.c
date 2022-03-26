fc0f8c26b3ef6d9d22e99655e94c2c07a78f7a99
HermelaB <hermelabrhanu@gmail.com>
2022-03-26 04:55:59 -0700

    Task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,28 +1,26 @@
 #include "main.h"
+
 /**
- * print_diagonal - prints diagonal
+ * print_diagonal - prints a diagonal line of length n
+ * @n: length of diagonal
  *
- * @n: integer to set diagonal
+ * Return: void
  */
+
 void print_diagonal(int n)
 {
-	int i;
-	int j;
+	int a, b;
 
 	if (n <= 0)
+		_putchar('\n');
+	for (a = 0; a < n; a++)
 	{
+		for (b = 0; b < a; b++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
 		_putchar('\n');
 	}
-	else
-	{
-		for (i = 0; i < n; i++)
-		{
-			for (j = 0; j < i; j++)
-			{
-				_putchar(' ');
-			}
-			_putchar(\\');
-			_putchar('\n');
-		}
-	}
+
 }
