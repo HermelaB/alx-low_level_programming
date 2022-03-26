92a78f7b2e2f9978799e267f332b18e95d884c8f
HermelaB <hermelabrhanu@gmail.com>
2022-03-26 05:38:51 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,29 +1,28 @@
 #include "main.h"
-
 /**
- * cap_string - capitalizes all words in a string
- * @s: string to capitalize
- *
- * Return: address of s
+ * cap_string - function that capitalizes all words of a string.
+ * @s: string
+ * Return: output
  */
 char *cap_string(char *s)
 {
-	int i = 0, j;
-	char a[] = " \t\n,;.!?\"(){}";
+	int i = 0;
 
-	while (*(s + i))
+	if (s[i] >= 97 && s[i] <= 122)
 	{
-		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		s[i] -= 32;
+	}
+	while (s[i] != '\0')
+	{
+		if (s[i] == ' ' || s[i] == '\n' || s[i] == '\t'
+		    || s[i] == ',' || s[i] == ';' || s[i] == '.'
+		    || s[i] == '!' || s[i] == '?' || s[i] == '"'
+		    || s[i] == '(' || s[i] == ')' || s[i] == '{'
+		    || s[i] == '}')
 		{
-			if (i == 0)
-				*(s + i) -= 'a' - 'A';
-			else
+			if (s[i + 1] >= 97 && s[i + 1] <= 122)
 			{
-				for (j = 0; j <= 12; j++)
-				{
-					if (a[j] == *(s + i - 1))
-						*(s + i) -= 'a' - 'A';
-				}
+				s[i + 1] -= 32;
 			}
 		}
 		i++;
